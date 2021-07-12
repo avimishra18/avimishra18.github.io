9b87400c103dde6a8c304fbd5a7726f277dad1b8
Avi Mishra <1828058@kiit.ac.in>
2021-07-12 23:51:12 +0530

    🌐 Added ContactSection

---
+++ src/components/common/ContactItem.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Grid, Typography } from "@material-ui/core";
+
+function ContactItem({ SVG, label, value }) {
+  return (
+    <Grid
+      container
+      direction="row"
+      alignItems="center"
+      style={{ marginTop: "7.5px", marginBottom: "7.5px" }}
+    >
+      <Grid item style={{ marginRight: "15px" }}>
+        <SVG color="primary" />
+      </Grid>
+      <Grid item>
+        <Grid container direction="column" justify="center">
+          <Grid item>
+            <Typography color="textSecondary">{label}</Typography>
+          </Grid>
+          <Grid item>
+            <Typography color="primary">{value}</Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default ContactItem;
