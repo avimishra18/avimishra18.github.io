1d6aaf6392e6a5fb9bf9b5f2b42ce407450da23b
Avi Mishra <1828058@kiit.ac.in>
2021-07-12 23:50:33 +0530

    🏗️ Added json file for storing data

--- src/components/sections/HomeSection.jsx
+++ src/components/sections/HomeSection.jsx
@@ -1,80 +1,184 @@
 import React from "react";
-import CoderSVG from "../../../static/hello_coder.svg";
-import { Grid, Typography, Box, Button } from "@material-ui/core";
+import {
+  Grid,
+  Typography,
+  Box,
+  Button,
+  Hidden,
+  IconButton,
+} from "@material-ui/core";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import MailIcon from "@material-ui/icons/Mail";
+import NavBarSpacing from "../common/NavBarSpacing";
+import basicInformation from "../../content/basic_information.json";
+import CoderSVG from "../../assets/hello_coder.svg";
 
-function HomeIntroduction() {
+function HomeIntroduction({ isMdUp, isSmUp }) {
+  const {
+    first_name_upper_case,
+    last_name_upper_case,
+    email,
+    github_url,
+    linkedin_url,
+    salutation,
+    profession,
+    short_introduction,
+  } = basicInformation;
+
   return (
-    <div>
-      <Typography variant="h3">Namaste! I'm-</Typography>
-      <Typography variant="h1" color="primary">
-        &nbsp; &nbsp; AVI MISHRA
-      </Typography>{" "}
-      <br />
-      <Typography variant="h4" color="textSecondary">
-        <Typography variant="inherit" color="primary">
-          <Box component="span" fontWeight="fontWeightBold">
-            Software Engineer.&nbsp;
-          </Box>
-        </Typography>
-        I specialize in developing Websites and Android Applications. Currently,
-        interning as a front-end developer.
-      </Typography>
-      <br />
-      <Grid container alignItems="center" sm={12} md={8} lg={6} wrap={true}>
-        <Grid item>
-          <Button
-            color="primary"
-            variant="outlined"
-            className="social-media-icons"
-            href="https://www.linkedin.com/avimishra18"
-            target="_blank"
-            children="Linkedin"
-            startIcon={<FontAwesomeIcon size="2x" icon={faLinkedin} />}
-            style={{ marginRight: "12px" }}
-          />
-        </Grid>
-        <Grid item>
-          <Button
-            color="primary"
-            variant="outlined"
-            className="social-media-icons"
-            href="https://github.com/avimishra18"
-            target="_blank"
-            children="Github"
-            startIcon={<FontAwesomeIcon size="2x" icon={faGithub} />}
-            style={{ marginRight: "12px" }}
-          />
-        </Grid>
-        <Grid item>
-          <Button
-            color="primary"
-            variant="outlined"
-            className="social-media-icons"
-            href="mailto:mishra.avi18@gmail.com"
-            target="_blank"
-            children="Email"
-            startIcon={<MailIcon />}
-          />
-        </Grid>
+    <Grid id="hehe" container direction="column">
+      {isMdUp ? null : (
+        <>
+          <NavBarSpacing />
+          <NavBarSpacing />
+        </>
+      )}
+      <Grid item xs={12}>
+        <Typography variant="h3">{salutation}</Typography>
       </Grid>
-    </div>
+      <Grid item xs={12}>
+        <Typography variant="h1" color="primary">
+          &nbsp; &nbsp;{" "}
+          {first_name_upper_case + (isSmUp ? " " + last_name_upper_case : "")}
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Typography variant={isMdUp ? "h4" : "h5"} color="textSecondary">
+          <Typography variant="inherit" color="primary">
+            <Box component="span" fontWeight="fontWeightBold">
+              {profession}&nbsp;
+            </Box>
+          </Typography>
+          {short_introduction}
+        </Typography>
+      </Grid>
+      <Grid item>
+        <br />
+      </Grid>
+      <Hidden xsDown>
+        <Grid
+          container
+          alignItems="center"
+          sm={12}
+          md={8}
+          lg={6}
+          wrap={true}
+          spacing={2}
+        >
+          <Grid item>
+            <Button
+              color="primary"
+              variant="outlined"
+              className="social-media-icons"
+              href={linkedin_url}
+              target="_blank"
+              children="Linkedin"
+              startIcon={<FontAwesomeIcon size="2x" icon={faLinkedin} />}
+            />
+          </Grid>
+          <Grid item>
+            <Button
+              color="primary"
+              variant="outlined"
+              className="social-media-icons"
+              href={github_url}
+              target="_blank"
+              children="Github"
+              startIcon={<FontAwesomeIcon size="2x" icon={faGithub} />}
+            />
+          </Grid>
+          <Grid item>
+            <Button
+              color="primary"
+              variant="outlined"
+              className="social-media-icons"
+              href={`mailto:${email}`}
+              target="_blank"
+              children="Email"
+              startIcon={<MailIcon />}
+            />
+          </Grid>
+        </Grid>
+      </Hidden>
+      <Hidden smUp>
+        <Grid container alignItems="center" justify="center">
+          <Grid item>
+            <IconButton
+              color="primary"
+              variant="outlined"
+              className="social-media-icons"
+              href={linkedin_url}
+              target="_blank"
+              children={<FontAwesomeIcon size="1x" icon={faLinkedin} />}
+            />
+          </Grid>
+          <Grid item>
+            <IconButton
+              color="primary"
+              variant="outlined"
+              className="social-media-icons"
+              href={github_url}
+              target="_blank"
+              children={<FontAwesomeIcon size="1x" icon={faGithub} />}
+            />
+          </Grid>
+          <Grid item>
+            <IconButton
+              color="primary"
+              variant="outlined"
+              className="social-media-icons"
+              href={`mailto:${email}`}
+              target="_blank"
+              children={<MailIcon />}
+            />
+          </Grid>
+        </Grid>
+      </Hidden>
+    </Grid>
   );
 }
 
 function HomeSection() {
+  const isSmUp = useMediaQuery(theme => theme.breakpoints.up("sm"));
+  const isMdUp = useMediaQuery(theme => theme.breakpoints.up("md"));
+
+  // Breakpoints Commit
+  console.log(
+    "xs",
+    useMediaQuery(theme => theme.breakpoints.only("xs"))
+  );
+  console.log(
+    "sm",
+    useMediaQuery(theme => theme.breakpoints.only("sm"))
+  );
+  console.log(
+    "md",
+    useMediaQuery(theme => theme.breakpoints.only("md"))
+  );
+  console.log(
+    "lg",
+    useMediaQuery(theme => theme.breakpoints.only("lg"))
+  );
+  console.log(
+    "xl",
+    useMediaQuery(theme => theme.breakpoints.only("xl"))
+  );
+
   return (
-    <div id="home" style={{ height: "100vh", width: "90vw" }}>
+    <div
+      id="home"
+      style={{ height: isMdUp ? "100vh" : "", width: isMdUp ? "90vw" : "" }}
+    >
       <Grid
         container
-        justify="space-evenly"
+        justify={isMdUp ? "space-evenly" : "center"}
         alignItems="center"
         style={{ height: "100%" }}
       >
         <Grid item sm={12} md={8} xl={6}>
-          <HomeIntroduction />
+          <HomeIntroduction isMdUp={isMdUp} isSmUp={isSmUp} />
         </Grid>
         <Grid item sm={12} md={4} xl={6}>
           <img src={CoderSVG} alt="Hello!" width="100%" />
