bd492cc3ac13906a3e2eac3a8b2bc0163ed5b404
Avi Mishra <1828058@kiit.ac.in>
2021-07-12 23:46:32 +0530

    ➕ Added Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,42 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, Typography, Box } from "@material-ui/core";
+import { useTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 function Footer() {
+  const theme = useTheme();
+  const isSmUp = useMediaQuery(theme => theme.breakpoints.up("sm"));
+
   return (
-    <Grid container alignItems="center" justify="center">
-      Built with Gatsby
+    <Grid
+      container
+      direction={isSmUp ? "row" : "column"}
+      justify="center"
+      alignItems="center"
+      style={{
+        marginTop: "25px",
+        padding: "10px 0px",
+        width: "100%",
+        borderTop: "1px solid rgba(0,0,0,.11)",
+        background: theme.palette.background.paper,
+      }}
+    >
+      <Typography variant="body2">
+        Built with{" "}
+        <Typography variant="inherit" color="primary">
+          Gatsby{" "}
+        </Typography>
+        &{" "}
+        <Typography variant="inherit" color="primary">
+          Material-UI. &nbsp;
+        </Typography>
+      </Typography>
+      <Typography variant="body2">
+        Designed & Developed by{" "}
+        <Typography variant="inherit" color="primary">
+          Avi Mishra.
+        </Typography>
+      </Typography>
     </Grid>
   );
 }
