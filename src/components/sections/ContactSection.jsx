cedd2ff6f4cbbe9ef1b04581a29b0a0e5a7ab6ec
Avi Mishra <1828058@kiit.ac.in>
2021-07-13 23:51:36 +0530

    📄 Added contact form URL

--- src/components/sections/ContactSection.jsx
+++ src/components/sections/ContactSection.jsx
@@ -11,61 +11,71 @@ import SchoolIcon from "@material-ui/icons/School";
 import basicInformation from "../../content/basic_information.json";
 
 function ContactForm() {
+  const { contact_form_url } = basicInformation;
   const textFieldTopMargin = "15px";
 
   return (
-    <Grid container direction="column">
-      <Typography variant="subtitle1" color="textSecondary">
-        Drop me a message
-      </Typography>
-      <Grid container spacing={2} justify="space-between">
-        <Grid item xs={6}>
-          <TextField
-            label="Name"
-            variant="outlined"
-            color="primary"
-            autoComplete="name"
-            required
-            fullWidth
-            style={{ marginTop: textFieldTopMargin }}
-          />
+    <form action={contact_form_url} method="POST">
+      <Grid container direction="column">
+        <Typography variant="subtitle1" color="textSecondary">
+          Drop me a message
+        </Typography>
+        <Grid container spacing={2} justify="space-between">
+          <Grid item xs={6}>
+            <TextField
+              type="name"
+              name="name"
+              label="Name"
+              variant="outlined"
+              color="primary"
+              autoComplete="name"
+              required
+              fullWidth
+              style={{ marginTop: textFieldTopMargin }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              type="email"
+              name="email"
+              label="Email"
+              variant="outlined"
+              color="primary"
+              autoComplete="email"
+              required
+              fullWidth
+              style={{ marginTop: textFieldTopMargin }}
+            />
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <TextField
-            label="Email"
-            variant="outlined"
-            color="primary"
-            autoComplete="email"
-            required
-            fullWidth
-            style={{ marginTop: textFieldTopMargin }}
-          />
-        </Grid>
-      </Grid>
 
-      <TextField
-        label="Message"
-        variant="outlined"
-        color="primary"
-        required
-        fullWidth
-        size="medium"
-        multiline
-        rows={3}
-        style={{ marginTop: textFieldTopMargin }}
-      />
-      <Grid container justify="flex-end">
-        <Grid item>
-          <Button
-            color="primary"
-            variant="contained"
-            children="Send"
-            startIcon={<SendIcon />}
-            style={{ marginTop: textFieldTopMargin }}
-          />
+        <TextField
+          type="message"
+          name="message"
+          label="Message"
+          variant="outlined"
+          color="primary"
+          required
+          fullWidth
+          size="medium"
+          multiline
+          rows={3}
+          style={{ marginTop: textFieldTopMargin }}
+        />
+        <Grid container justify="flex-end">
+          <Grid item>
+            <Button
+              type="submit"
+              color="primary"
+              variant="contained"
+              children="Send"
+              startIcon={<SendIcon />}
+              style={{ marginTop: textFieldTopMargin }}
+            />
+          </Grid>
         </Grid>
       </Grid>
-    </Grid>
+    </form>
   );
 }
 
