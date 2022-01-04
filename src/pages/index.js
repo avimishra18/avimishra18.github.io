91157cabbf3838a736f89410034d157accf636ce
Avi Mishra <1828058@kiit.ac.in>
2022-01-04 20:30:50 +0530

    ⚡️ Self-hosted Google Fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,10 @@ import {
   PROJECTS_SECTION,
   CONTACT_SECTION,
 } from "../utils/constants";
+import "@fontsource/inter/300.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/900.css";
 
 export default function Home() {
   const { name_proper_case } = basicInformation;
